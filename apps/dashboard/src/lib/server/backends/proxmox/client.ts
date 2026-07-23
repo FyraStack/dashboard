73232f1b39140d842c6d8d6b4740cf9b99a3f5c2
Cappy Ishihara <cappy@cappuchino.xyz>
2026-07-23 22:12:52 +0700

    disable auto-retry for fetching network interfaces
    For some reason fetching network interfaces return an error Ky considers
    "transient" when trying to fetch network interfaces, causing some
    request times to compound to up to 10s for this specific endpoint
    because Ky retries 3 times, each time costing ~3.3s silently ballooning
    request times to 10s

--- apps/dashboard/src/lib/server/backends/proxmox/client.ts
+++ apps/dashboard/src/lib/server/backends/proxmox/client.ts
@@ -412,8 +412,11 @@ export class ProxmoxClient {
 	// Guest agent
 
 	async getNetworkInterfaces(node: string, vmid: number): Promise<PveAgentNetworkInterface[]> {
-		const res = await this.api
-			.get(`nodes/${encodeURIComponent(node)}/qemu/${vmid}/agent/network-get-interfaces`)
+    const res = await this.api
+      // diable retry for fetching network interfaces
+      // causes performance issues on querying VM network interfaces, ky retries 3x then gives up anyway
+      // so we give up early after first attempt
+			.get(`nodes/${encodeURIComponent(node)}/qemu/${vmid}/agent/network-get-interfaces`, { retry: 0 })
 			.json<PveResponse<{ result: PveAgentNetworkInterface[] }>>();
 		return res.data.result;
 	}
